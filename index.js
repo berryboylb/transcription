0eed3332b03201dc6a0f35c30e04305755bbfb58
berryboylb <phemmynesce4life@gmail.com>
2023-10-02 09:34:51 +0100

    @chore: chnages

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ app.post("/transcribe", async (req, res) => {
 });
 
 const videoFolder = path.join(__dirname, "uploads/videos/");
+const imageFolder = path.join(__dirname, "uploads/images/");
 app.get("/videos/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(videoFolder, fileName);
@@ -115,6 +116,64 @@ app.get("/videos/:fileName", (req, res) => {
       fileStream.pipe(res);
     }
   });
+});
+
+// Middleware to handle incoming video chunks
+app.post("/upload-video-stream", (req, res) => {
+  const videoFileName = "uploaded-video.webm"; // Set the desired file name
+  const videoFilePath = path.join(videoFolder, videoFileName);
+  const writeStream = fs.createWriteStream(videoFilePath, { flags: "a" });
+  req.on("data", (chunk) => {
+    writeStream.write(chunk);
+  });
+  req.on("end", () => {
+    writeStream.end();
+    res.setHeader("Content-Type", "video/webm");
+    const uniqueVideoId = uuid();
+    const uniqueVideoFileName = `${uniqueVideoId}.webm`;
+    fs.renameSync(videoFilePath, path.join(videoFolder, uniqueVideoFileName));
+    res.status(200).json({ message: "Video upload completed." });
+  });
+  req.on("error", (err) => {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred during video upload." });
+  });
+});
+
+const uploadFile = (req, filePath) => {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createWriteStream(filePath, { flags: "a" });
+    stream.setDefaultEncoding("binary");
+    stream.on("open", () => {
+      console.log("Stream open ...  0.00%");
+      req.pipe(stream);
+    });
+
+    stream.on("drain", () => {
+      const written = parseInt(stream.bytesWritten);
+      const total = parseInt(req.headers["content-length"]);
+      const pWritten = ((written / total) * 100).toFixed(2);
+      console.log(`Processing  ...  ${pWritten}% done`);
+    });
+
+    stream.on("close", () => {
+      console.log("Processing  ...  100%");
+      resolve(filePath);
+    });
+
+    stream.on("error", (err) => {
+      console.error(err);
+      reject(err);
+    });
+  });
+};
+
+app.post("/test", (req, res) => {
+  const name = `${uuid()}.webm`;
+  const filePath = path.join(__dirname, `/uploads/videos/${name}`);
+  uploadFile(req, filePath)
+    .then((path) => res.send({ status: "success", path, name }))
+    .catch((err) => res.send({ status: "error", err }));
 });
 
 app.use(errorHandler);
